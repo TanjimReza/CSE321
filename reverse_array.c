acd2ad5f041fc654e674aa0ce87cb38914c24dfc
Tanjim Reza <35164789+TanjimReza@users.noreply.github.com>
2023-10-14 21:35:06 +0600

    Create README.MD

---
+++ reverse_array.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int my_array[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9 ,10};
+    int reversed_array[10] = {};
+
+    for (int i = 0; i < 10; i++) {
+        reversed_array[i] = my_array[9 - i];
+    }
+
+
+    for (int i = 0; i < 10; i++) {
+        printf("%d ", reversed_array[i]);
+    }
+    return 0;
+}   
